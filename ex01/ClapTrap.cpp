540db25d51abe67eefa9b4f695a42c44653c1212
wuabdull <164627325+WorodHazim@users.noreply.github.com>
2026-02-24 03:52:19 +0400

    Add default constructor to ClapTrap class

--- ex01/ClapTrap.cpp
+++ ex01/ClapTrap.cpp
@@ -13,6 +13,12 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
+ClapTrap::ClapTrap()
+  : _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap default constructed" << std::endl;
+}
+
 ClapTrap::ClapTrap(const ClapTrap& other)
     : _name(other._name),
       _hitPoints(other._hitPoints),
