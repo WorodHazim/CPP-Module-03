718f7422da9a8c489d2438d8e0feaca205b989ac
wuabdull <164627325+WorodHazim@users.noreply.github.com>
2026-02-24 04:06:27 +0400

    Implement default constructor for ClapTrap class

--- ex02/ClapTrap.cpp
+++ ex02/ClapTrap.cpp
@@ -13,6 +13,12 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
+ClapTrap::ClapTrap()
+  : _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap default constructed" << std::endl;
+}
+
 ClapTrap::ClapTrap(const ClapTrap& other)
     : _name(other._name),
       _hitPoints(other._hitPoints),
@@ -66,7 +72,10 @@ void ClapTrap::attack(const std::string& target)
 void ClapTrap::takeDamage(unsigned int amount)
 {
     if (_hitPoints <= 0)
+    {
+        std::cout << "ClapTrap " << _name << " is already dead" << std::endl;
         return;
+    }
 
     if (_hitPoints <= (int)amount)
         _hitPoints = 0;
